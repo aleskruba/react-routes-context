5b4f2956ec9825c23b192bfae33547491a8d591a
aleskruba <74367962+aleskruba@users.noreply.github.com>
2023-03-13 13:22:54 +0100

    Create RequireAuth.js

---
+++ components/RequireAuth.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { useLocation,Navigate,Outlet } from 'react-router-dom';
+import useAuth from './hooks/useAuth';
+
+
+const RequireAuth = () => {
+  const {auth} = useAuth();
+  const location = useLocation();
+
+  return (
+    auth ? <Outlet/> : <Navigate to="/login"  state={{from:location}} replace/>
+  )
+}
+
+
+export default RequireAuth
