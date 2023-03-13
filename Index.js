8a15c64a2a000705abaa0396a21b6bf12f8161b8
aleskruba <74367962+aleskruba@users.noreply.github.com>
2023-03-13 13:19:46 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,27 @@
+import * as React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import App from './App';
+import { AuthProvider} from './components/hooks/Context';
+
+
+
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+
+root.render(
+ 
+  <BrowserRouter>
+    <AuthProvider>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+       </AuthProvider>    
+  </BrowserRouter>
+  
+);
 
