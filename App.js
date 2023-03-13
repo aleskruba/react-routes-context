b19b9ee7207e20d8b833ce351039b1dcfce296c9
aleskruba <74367962+aleskruba@users.noreply.github.com>
2023-03-13 13:19:27 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,37 @@
+import * as React from 'react';
+import { Form } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import './style.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Missing from './components/Missing';
+import RequireAuth from './components/RequireAuth';
 
+
+export default function App() {
+
+
+
+  return (
+    <div>
+ 
+        <Routes>
+            <Route path="/" element={<Layout />}>
+          
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+
+
+              <Route element={<RequireAuth/>}>
+                <Route path="home" element={<Home />} />
+              </Route>
+
+              <Route path="*" element={<Missing/>} />
+            </Route>
+        </Routes>
+
+    </div>
+  );
+}
